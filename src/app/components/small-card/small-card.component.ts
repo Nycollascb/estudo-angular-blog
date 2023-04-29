b5a35ef82bf87d6d4f6d9b426612e37a9d0f0c2e
Nycollas Chapur <nyckycb@gmail.com>
2023-04-28 23:19:51 -0300

    cards dinamicos

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-small-card',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  photoCover: string = ''
+  @Input()
+  cardTitle: string = ''
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
